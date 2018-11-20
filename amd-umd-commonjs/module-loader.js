c0a870a899ec432cbafe4efee132e07bfe908454
Henry Cho <comprocho@gmail.com>
2018-11-20 05:03:36 +0000

    updated

--- amd-umd-commonjs/module-loader.js
+++ amd-umd-commonjs/module-loader.js
@@ -4,16 +4,83 @@ https://fem-node-api.netlify.com/
 https://www.davidbcalhoun.com/2014/what-is-amd-commonjs-and-umd/
 http://www.commonjs.org/specs/modules/1.0/
 https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/Using#Importing_CommonJS_modules
-
--CommonJS: Module loader. Node uses CommonJS for its module loader.
--Using require() we can get access to build-in and 3rd party npm modules.
 */
 
-// built in node module
-var path = require('path');
+/***
+CommonJS
+    *keywords: require and exports
+    *NodeJS implements CommonJS specificationf or its module loader
+    *Synchronous and circular dependencies are supported
+    *Browserify uses CommonJS in browser
+***/
 
-// 3rd party module downloaded into node_modules/
-var _ = require('lodash');
+//payments.js
+var customerStore = require('store/customer'); // import module
 
-// a module we created in another file
-var myModule = require('./path/to/my/module');
+//store/customer.js
+function customerStore () {
+    return customers.get('store);
+}
+
+modules.exports = customerStore;
+
+
+/***
+Asynchronous Module Definition (AMD)
+    *keyword: define
+    *born as CommonJS was not suitable for browser
+    *asynchronous module loader
+    *RequireJS implements AMD
+***/
+
+define(['module1', ',module2'], function(module1, module2) {
+  console.log(module1.setName());
+  
+  function sum(a,b) {
+      return a+b;
+  } // private because it's not returned at the end
+  
+  function prod(a,b) {
+      return a*b;
+  } // public because it's returned at the end
+  
+  //exposed public methods
+  return {
+      prod: prod
+  }
+});
+
+//RequireJS
+<script data-main="scripts/main" src="scripts/require.js"></script>
+
+
+/***
+Universal Module definition (UMD)
+    *supports both AMD and CommonJS
+***/
+
+(function (window, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD style
+        define(['jquery', 'underscore'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS (NodeJS) style
+        module.exports = factory(require('jquery'), require('underscore'));
+    } else {
+        // Browser globals (on window)
+        window.returnExports = factory(window.jQuery, window._);
+    }
+}(this, function ($, _) {
+    function sum(a,b) {
+        return a+b;
+    } // private because it's not returned at the end
+    
+    function prod(a,b) {
+        return a*b;
+    } // public because it's returned at the end
+    
+    //exposed public methods
+    return {
+        prod: prod
+    }
+}));
